375c856d67aa831dc78ea069b3b75f49c7afa352
rumblefish2494 <rumbelfish2494@gmail.com>
2015-09-12 09:56:50 -0700

    added all sql commands as instructed by reviewer

--- tournament.sql
+++ tournament.sql
@@ -1,5 +1,12 @@
 -- Table definitions for the tournament project.
 
+
+-- from /tournament unix prompt
+psql
+-- from vagrant prompt
+create database tournament;
+--connect to new tournament database
+\c tournament
 -- create players table with id name wins and matches column
 create table players(id serial, name text, wins integer, matches integer);
 
